9a259db4a29c079b6d528a005ab9a80ac5bd197c
Dmitriy <dimmak.260823@gmail.com>
2016-07-28 22:32:26 +0300

    Add /visit page

--- app.rb
+++ app.rb
@@ -12,8 +12,30 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.order "created_at DESC"
+end
+
 
 get '/' do
-	@barbers = Barber.all
+	
 	erb :index			
+end
+
+get '/visit' do
+	
+   	erb :visit
+end
+
+post '/visit' do
+	
+	@name=params[:username]
+	@phone=params[:phone]
+	@datetime=params[:datetime]
+	@barber=params[:barber]
+	@color=params[:color]
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
+	
 end
